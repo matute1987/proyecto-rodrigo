6ab638ad5b95b43b04932a9be763fd8c5e4637cb
matute1987 <freirerodrigo@hotmail.com.ar>
2025-08-09 19:01:20 -0300

    Fix import de home.css y estructura de estilos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer style={{ backgroundColor: "lightcoral" }}>
-      <p>Sitio desarrollado por <a href="https://www.linkedin.com/in/gabriel-alberini/" target="_blank">Gabriel Alberini</a></p>
+      <p>Sitio desarrollado por <a href="https://www.linkedin.com/in/rodrigo-freire1987/" target="_blank">Freire Rodrigo</a></p>
     </footer>
   )
 }
